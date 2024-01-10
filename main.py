997204af40ee03be2301111e0a90cdc8ba34c742
Miikka Virtanen <32563239+MiiVirt@users.noreply.github.com>
2024-01-10 11:29:26 +0200

    Add files via upload
    Added encrypt and decrypt functions to secure the passwords being saved.

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import os, sys, csv
-from cryptography.hazmat.backends import  default_backend
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.fernet import Fernet
+
 
 def encrypt_data(data):
-    cipher = Cipher(algorithms.AES(), modes.CFB(), backend=default_backend())
-    encryptor = cipher.encryptor()
-    data = data.encode('utf-8')
-    encrypted_password = encryptor.update(data) + encryptor.finalize()
-    return encrypted_password
+    key = Fernet.generate_key()
+    cipher_suite = Fernet(key)
+    data_bytes = data.encode('utf-8')
+    encrypted_password = cipher_suite.encrypt(data_bytes)
+    return encrypted_password, key
+
+def decrypt_data(data, key):
+    cipher_suite = Fernet(key)
+    decrypted_password = cipher_suite.decrypt(data).decode('utf-8')
+    return decrypted_password
+
+
 def save_password(title, username, password):
     file_path = "passwords.txt"
     with open(file_path, 'a') as file:
@@ -26,6 +33,7 @@ def read_passwords():
     return data_list
 
 
+
 def main():
     title = input("Type the title: ")
     username = input("Type the username: ")
@@ -34,7 +42,11 @@ def main():
     data_list = read_passwords()
     for data_set in data_list:
         print(f"Title: {data_set['Title']}, Username: {data_set['Username']}, Password: {data_set['Password']}")
-
+    data = input("Give data")
+    encrypted_data, key = encrypt_data(data)
+    print(encrypted_data)
+    decrypted_data = decrypt_data(encrypted_data, key)
+    print(decrypted_data)
 
 if __name__ == "__main__":
     main()
