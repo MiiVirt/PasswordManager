0928ccceb285a97ceed3bdb434992718e8bc3925
Miikka Virtanen <32563239+MiiVirt@users.noreply.github.com>
2024-01-12 14:40:45 +0200

    Add files via upload
    Currently gives error, Most likely due to saving encrypted password and key in bytes but reads them as string.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os, sys, csv
 from cryptography.fernet import Fernet
 
+
 def generate_key(): #Generate key for encryption
     return Fernet.generate_key()
 
@@ -11,9 +12,11 @@ def encrypt_data(data, key):
     encrypted_password = cipher_suite.encrypt(data_bytes) #encrypts the data
     return encrypted_password
 
+
 def decrypt_data(data, key):
-    cipher_suite = Fernet(key) #creates cipher with the given key
-    decrypted_password = cipher_suite.decrypt(data).decode('utf-8') #decrypts and decodes the data
+    cipher_suite = Fernet(key)
+    decrypted_data = cipher_suite.decrypt(data)
+    decrypted_password = decrypted_data
     return decrypted_password
 
 
@@ -24,12 +27,8 @@ def save_password(title, username, password, key):
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         if csvfile.tell() == 0:
             writer.writeheader()
-        writer.writerow({'Title': title, 'Username': username, 'Password': password, 'Key': key.decode('utf-8')})
+        writer.writerow({'Title': title, 'Username': username, 'Password': password, 'Key': key})
     print("Password saved")
-
-
-def update_info():
-    file_path = "passwords.csv"
 
 
 def read_passwords():
@@ -42,24 +41,32 @@ def read_passwords():
     return data_list
 
 
-
 def main():
-    title = input("Type the title: ")
-    username = input("Type the username: ")
-    password = input("type the password:")
-    key = generate_key()
+    print("Press '1' to save new credentials")
+    print("Press '2' to see all currently saved credentials")
+    print("Press '3' to edit an existing credential")
+    response = input(": ")
+    if response == '1':
+        title = input("Type the title: ")
+        username = input("Type the username: ")
+        password = input("type the password:")
+        key = generate_key()
+        encrypted_data = encrypt_data(password, key)
+        save_password(title, username, encrypted_data, key) #NOTE encrypted_data and key are saved as bytes here
+    elif response == '2':
+        data_list = read_passwords()
+        passwords = []
+        keys = []
+        for data_set in data_list:
+            passwords.append(data_set['Password'])
+            keys.append(data_set['Key'])
+        decrypted_passwords = [decrypt_data(password, key) for password, key in zip(passwords, keys)]
+        for decrypted_data in decrypted_passwords:
+            print(decrypted_data)
 
-    encrypted_data = encrypt_data(password, key)
-    #print(encrypted_data)
-    save_password(title, username, encrypted_data, key)
+    elif response == '3':
+        return
 
-    data_list = read_passwords()
-    for data_set in data_list:
-        print(f"Title: {data_set['Title']}, Username: {data_set['Username']}, Password: {data_set['Password']}, Key: {data_set['Key']}")
-
-
-    decrypted_data = decrypt_data(encrypted_data, key)
-    print(decrypted_data)
 
 if __name__ == "__main__":
     main()
